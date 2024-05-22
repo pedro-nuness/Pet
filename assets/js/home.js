ff4e70176c64ec6171f16712102413e2a5877998
Nunes <93084039+pedro-nuness@users.noreply.github.com>
2024-05-22 17:40:37 -0300

    Update
    -Adicionando carrossel

--- assets/js/home.js
+++ assets/js/home.js
@@ -3,15 +3,24 @@ let CarrosselBox = document.getElementsByClassName("carrossel_movable_box");
 let CarrosselItems = document.getElementsByClassName("carrossel_item");
 
 const Items = [
-    [
+    [   
         {
             imagem: "assets/img/bag.png",
         },
         {
-            imagem: "assets/img/bag.png",
+            imagem: "assets/img/user.png",
         },
     ]
 ];
+
+let Indexes = [
+    {
+        index: 0,
+    },
+    {
+        index: 0,
+    },
+]
 
 function AdjustCarrosselSizing() {
     for (let i = 0; i < CarrosselBox.length; i++) {
@@ -21,20 +30,20 @@ function AdjustCarrosselSizing() {
 
 function AdjustItemsSize() {
     let CurrentIdx = 0;
-    for (let i = 0; i < Items.length; i++) {
-        console.log(i);
-        let boxWidth = CarrosselBox[i].style.width; // Obtenha a largura calculada em pixels
-        let itemWidth = boxWidth / Items[i].length;
+    for (let i = 0; i < Items.length ; i++) {
+        let itemWidth = 100 / Items[i].length;
         for (let j = 0; j < Items[i].length; j++) {
             CarrosselItems[CurrentIdx].style.width = itemWidth + '%';
             CurrentIdx++;
+            console.log(itemWidth);
         }
     }
 }
 
 function InsertItems(){
     for(let i =0; i < CarrosselBox.length; i++){
-        for(let j =0; j < Items[i].length; j++){
+        let j =0;
+        for(j =0; j < Items[i].length ; j++){
             let newItem = document.createElement('div');
             newItem.className = 'carrossel_item';
 
@@ -45,16 +54,25 @@ function InsertItems(){
             newItem.appendChild(CarrosselImg);
 
             CarrosselBox[i].appendChild(newItem);
-            console.log(1);
+            
         }
+        console.log("added " + j + " items into " + i);
     }
 }
 
-function MoveCarrossel(element, side){
-    let Box = document.getElementById(element);
+function MoveCarrossel(idx, side){
+    let CarrosselIDX = Indexes[idx].index;
+
     if(side){
-        
-    }
+        if(CarrosselIDX < Items[idx].length - 1) 
+            CarrosselIDX++;
+    }else if(CarrosselIDX > 0)
+        CarrosselIDX--;
+    console.log(CarrosselIDX);
+
+    CarrosselBox[idx].style.left = -CarrosselIDX * 100 + '%';   
+
+    Indexes[idx].index = CarrosselIDX;
 }
 
 InsertItems();
