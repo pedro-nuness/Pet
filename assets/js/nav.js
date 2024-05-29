11f821e9c2de4821b4a43d03328fc39a79dceab9
Nunes <93084039+pedro-nuness@users.noreply.github.com>
2024-05-29 11:55:52 -0300

    Update

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -16,3 +16,28 @@ function AdjustCounter(){
     }
 }
 setInterval(AdjustCounter, 0);
+
+let OptionsActive = false;
+
+function ChangeLateralBarState(){
+    
+    OptionsActive = !OptionsActive;
+    AdjustLateralBar();
+}
+
+function AdjustLateralBar(){
+    let LateralBar = document.getElementById("lateral_bar");
+    let OptionsButton = document.getElementById("options_button");
+    if(OptionsActive){       
+        OptionsButton.innerHTML = "close"; 
+        LateralBar.style.left = "50%"; 
+        LateralBar.style.width = "50vw";   
+    }
+    else{
+        OptionsButton.innerHTML = "density_medium";
+        LateralBar.style.width = "0vw";
+        LateralBar.style.left = "100%";      
+    }
+}
+
+
